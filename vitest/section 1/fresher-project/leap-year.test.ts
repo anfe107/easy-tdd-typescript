dfedce09ee9b27a30c396858c28cf38dd490bd53
Andreas <andreas.feser@posteo.net>
2025-03-13 13:37:03 +0100

    finnished the leap year algorithm in test first approach

--- vitest/section 1/fresher-project/leap-year.test.ts
+++ vitest/section 1/fresher-project/leap-year.test.ts
@@ -12,4 +12,16 @@ describe('leap year algorithm', () => {
   it('detects a century year as a non leap year', () => {
     expect(isLeapYear(1900)).toBeFalsy();
   });
+
+  it('detects year 0 to be a non leap year', () => {
+    expect(isLeapYear(0)).toBeFalsy();
+  });
+
+  it('detects year 2000 to be a leap year - divisible by 400', () => {
+    expect(isLeapYear(2000)).toBeTruthy();
+  });
+
+  it('detects negative years e.g. -4 to be a non leap year', () => {
+    expect(isLeapYear(-4)).toBeFalsy();
+  });
 });
--- vitest/section 1/fresher-project/leap-year.ts
+++ vitest/section 1/fresher-project/leap-year.ts
@@ -1,5 +1,10 @@
 const isDivisibleBy = (year: number, divisor: number): boolean =>
   year % divisor === 0;
 
-export const isLeapYear = (year: number): boolean =>
-  isDivisibleBy(year, 4) && !isDivisibleBy(year, 100);
+export const isLeapYear = (year: number): boolean => {
+  if (year < 1) return false;
+  return (
+    (isDivisibleBy(year, 4) && !isDivisibleBy(year, 100)) ||
+    isDivisibleBy(year, 400)
+  );
+};
